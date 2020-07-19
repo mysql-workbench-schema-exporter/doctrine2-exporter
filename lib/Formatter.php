ab8d5ec645e6d94a282e8628724026b55c14262a
Toha <tohenk@yahoo.com>
2020-07-19 21:11:12 +0700

    Bump version to 3.1.1.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- lib/Formatter.php
+++ lib/Formatter.php
@@ -4,7 +4,7 @@
  * The MIT License
  *
  * Copyright (c) 2010 Johannes Mueller <circus2(at)web.de>
- * Copyright (c) 2012-2014 Toha <tohenk@yahoo.com>
+ * Copyright (c) 2012-2020 Toha <tohenk@yahoo.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -98,7 +98,7 @@ abstract class Formatter extends BaseFormatter
 
     public function getVersion()
     {
-        return 'dev';
+        return '3.1.1';
     }
 
     /**
